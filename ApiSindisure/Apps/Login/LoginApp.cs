0ca8062aa8ad5356d7ac74e69ac3f745d7aa34d5
Cleiton Lopes <41643385+Cln2119@users.noreply.github.com>
2025-08-14 10:50:12 -0300

    Ajuste para fazer o Auth pelo back

--- ApiSindisure/Apps/Login/LoginApp.cs
+++ ApiSindisure/Apps/Login/LoginApp.cs
@@ -19,43 +19,95 @@ namespace ApiSindisure.Apps.Login
             _jwtTokenGenerator = jwtTokenGenerator;
         }
 
-        public async Task<LoginViewModel.Response> HandleAsync(LoginViewModel.Request request, CancellationToken cancellationToken)
+       public async Task<LoginViewModel.Response> HandleAsync(LoginViewModel.Request request, CancellationToken cancellationToken)
         {
             try
             {
                 var session = await _supabaseService.SignIn(request.Email, request.Password);
 
-                if (session == null)
-                {
-                    return new LoginViewModel.Response
-                    {
-                        Error = "Credenciais inválidas"
-                    };
-                }
-
                 var user = await _supabaseService.GetUser(session.AccessToken);
 
-                var token = _jwtTokenGenerator.GenerateToken(session.User.Id);
-                
-                var name = LerTokenJwtHelper.GetFisrtName(session.AccessToken);
-                var confirmationMail = LerTokenJwtHelper.GetConfirmationMail(session.AccessToken);
-
+                // Mapeando dados do Supabase -> LoginViewModel.Response
                 var response = new LoginViewModel.Response
                 {
-                    User = new UserDto
+                    AccessToken = session.AccessToken,
+                    TokenType = session.TokenType,
+                    ExpiresIn = session.ExpiresIn,
+                    ExpiresAt = new DateTimeOffset(session.ExpiresAt()).ToUnixTimeSeconds(),
+                    RefreshToken = session.RefreshToken,
+                    User = new LoginViewModel.UserResponse
                     {
-                        Id = session.User.Id,
-                        Nome = name,
-                        Email = session.User.Email,
-                        Email_confirmed_at = confirmationMail
-                    },
-                    Session = new SessionDto
-                    {
-                        AccessToken = session.AccessToken,
-                        ExpiresAt = DateTime.UtcNow.AddHours(1)
-                    },
-                    WeakPassword = false,
-                    Error = null
+                        Id = user.Id,
+                        Aud = user.Aud,
+                        Role = user.Role,
+                        Email = user.Email,
+                        EmailConfirmedAt = user.EmailConfirmedAt,
+                        Phone = user.Phone,
+                        ConfirmedAt = user.ConfirmedAt,
+                        LastSignInAt = user.LastSignInAt,
+                        AppMetadata = new LoginViewModel.AppMetadata
+                        {
+                            Provider  = user.AppMetadata?["provider"]?.ToString(),
+                            Providers = user.AppMetadata?["providers"] as List<string> 
+                                ?? new List<string>()
+                        },
+                        UserMetadata = new LoginViewModel.UserMetadata
+                        {
+                            Address        = user.UserMetadata?["address"]?.ToString(),
+                            Cep            = user.UserMetadata?["cep"]?.ToString(),
+                            City           = user.UserMetadata?["city"]?.ToString(),
+                            DateOfBirth    = Convert.ToDateTime(user.UserMetadata?["date_of_birth"]?.ToString()),
+                            DocumentNumber = user.UserMetadata?["document_number"]?.ToString(),
+                            Email          = user.UserMetadata?["email"]?.ToString(),
+                            EmailVerified  = user.UserMetadata?["email_verified"] != null && 
+                                            bool.TryParse(user.UserMetadata["email_verified"].ToString(), out var ev) ? ev : false,
+                            FirstName      = user.UserMetadata?["first_name"]?.ToString(),
+                            LastName       = user.UserMetadata?["last_name"]?.ToString(),
+                            Neighborhood   = user.UserMetadata?["neighborhood"]?.ToString(),
+                            Number         = user.UserMetadata?["number"]?.ToString(),
+                            Phone          = user.UserMetadata?["phone"]?.ToString(),
+                            PhoneVerified  = user.UserMetadata?["phone_verified"] != null && 
+                                            bool.TryParse(user.UserMetadata["phone_verified"].ToString(), out var pv) ? pv : false,
+                            State          = user.UserMetadata?["state"]?.ToString(),
+                            Sub            = user.UserMetadata?["sub"]?.ToString(),
+
+                        },
+                        Identities = user.Identities?.Select(i => new LoginViewModel.Identity
+                        {
+                            IdentityId = i.IdentityId,
+                            Id = i.Id,
+                            UserId = i.UserId,
+                            IdentityData = new LoginViewModel.UserMetadata
+                            {
+                               Address        = i.IdentityData?["address"]?.ToString(),
+                                Cep            = i.IdentityData?["cep"]?.ToString(),
+                                City           = i.IdentityData?["city"]?.ToString(),
+                                DateOfBirth    = Convert.ToDateTime(i.IdentityData?["date_of_birth"]?.ToString()),
+                                DocumentNumber = i.IdentityData?["document_number"]?.ToString(),
+                                Email          = i.IdentityData?["email"]?.ToString(),
+                                EmailVerified  = i.IdentityData?["email_verified"] != null &&
+                                                bool.TryParse(i.IdentityData["email_verified"].ToString(), out var iev) ? iev : false,
+                                FirstName      = i.IdentityData?["first_name"]?.ToString(),
+                                LastName       = i.IdentityData?["last_name"]?.ToString(),
+                                Neighborhood   = i.IdentityData?["neighborhood"]?.ToString(),
+                                Number         = i.IdentityData?["number"]?.ToString(),
+                                Phone          = i.IdentityData?["phone"]?.ToString(),
+                                PhoneVerified  = i.IdentityData?["phone_verified"] != null &&
+                                                bool.TryParse(i.IdentityData["phone_verified"].ToString(), out var ipv) ? ipv : false,
+                                State          = i.IdentityData?["state"]?.ToString(),
+                                Sub            = i.IdentityData?["sub"]?.ToString(),
+
+                            },
+                            Provider = i.Provider,
+                            LastSignInAt = i.LastSignInAt,
+                            CreatedAt = i.CreatedAt,
+                            UpdatedAt = i.UpdatedAt,
+                            Email = i.IdentityData?["email"]?.ToString(),
+                        }).ToList(),
+                        CreatedAt = user.CreatedAt,
+                        UpdatedAt = user.UpdatedAt,
+                        IsAnonymous = user.IsAnonymous
+                    }
                 };
 
                 return response;
@@ -64,7 +116,7 @@ namespace ApiSindisure.Apps.Login
             {
                 return new LoginViewModel.Response
                 {
-                    Error = "Erro interno do servidor"
+                    ErrorMessage = "Erro interno do servidor"
                 };
             }
         }
